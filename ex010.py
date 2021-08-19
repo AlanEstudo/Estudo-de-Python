5920c7e1599f0c625408202d03f5a412547bfd43
AlanEstudo <alanjuniorestudo@gmail.com>
2021-08-19 06:14:15 -0300

    Exercicio 010 (Compra de Dolares)

--- ex010.py
+++ ex010.py
@@ -1,2 +1,8 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos dolares ela pode comprar,
-#considere U$$1.00 = R$:3,27
+# considere U$$1.00 = R$:3,27
+
+vlDinheiro = float(input('Valor em Real :'))
+
+vlDolar = vlDinheiro * 3.27
+
+print('Com {} reais voce pode comprar {:.2f} de dolares'.format(vlDinheiro, vlDolar))
