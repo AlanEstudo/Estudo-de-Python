0fa818c653d6ed8fc502d0385e4514cee225dea6
AlanEstudo <alanjuniorestudo@gmail.com>
2021-08-23 06:21:29 -0300

    Manipulando textos

---
+++ aula09a.py
@@ -0,0 +1,20 @@
+# Manipulando texto
+# frase[4] retorna o caracter na posição do ponteiro
+# frase[:13] retorna do inicio ao 13
+# frase[1:15:2] retorna os caracter do 1 ao 15 pulando de 2 em 2
+# len(frase) retorna o tamanho da string
+# frase,count('a') retorna a quantidade do caracter
+# frase.find('deo') retorna qual a possição do ponteiro começoou
+# 'curso' in frase retorna true ou false dentro da string
+# frase.replace('Phyton', 'Android') substitui a 1 string pela segunda
+# frase.upper() trasnforma todas as letras em maiuscula
+# frase.lower() transforma todas as letras em minuscula
+# frase.capitalize() transforma toda a string a minuscula e a primeira letra em Maiuscula
+# frase.title() transcaforma todos os 1ª caracter das palavras em maiuscula
+# frase.strip() remove os espaços no inicio e no fim da string
+# frase.rstrip() remove apenas os ultimos espaços da string
+# frase.lstrip() remove apenas os primeiros espaços da string
+# frase.split() divide as string e gera uma lista de palavras
+# '-'.join(frase) junta uma cadeia de caracter colocando o - entre elas
+# print(""" Texto """) retorna o paragrafo inteiro
+
