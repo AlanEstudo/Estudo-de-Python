3554fda51ebd05d513a83ca3c70cd7f4d6830784
AlanEstudo <alanjuniorestudo@gmail.com>
2021-08-24 06:19:21 -0300

    Procurando uma string dentro de outra

---
+++ ex025.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia o nome de uma pessoa e diga se eka tem "Silva" no nome.
+
+vlNome = str(input('Nome: ')).strip()
+
+print('Tem "SILVA" no nome? {}'.format('SILVA' in vlNome.upper()))
+
