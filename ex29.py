c0a364fd7d13a032e341e7923fc3ce8a26770d19
AlanEstudo <alanjuniorestudo@gmail.com>
2021-08-26 05:54:12 -0300

    Impar ou Par

--- ex29.py
+++ ex29.py
@@ -2,10 +2,11 @@
 # Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado
 # A multa vai custar R$7,00 por cada km acima do limite
 
-vlKm = int(input('Velocidade do carro: '))
+vlKm = float(input('Velocidade do carro: '))
 vlAcima = vlKm - 80
 vlCalculo = (vlKm - 80) * 7
 
 if vlKm > 80:
     print('Você foi multado! O limite de velocidade é 80km/h e você esta {}km/h a cima da velocidade'.format(vlAcima))
     print('A multa vai custar R$7,00 por km ultrapassado,\nValor a pagar : R${:.2f}'.format(vlCalculo))
+print('Tenha um bom dia! Digija com segurança!')
