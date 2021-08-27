2d305a4a0af4808d70e9ba969a52107d59e393df
AlanEstudo <alanjuniorestudo@gmail.com>
2021-08-27 06:23:07 -0300

    Analisando Triângulo

---
+++ ex035.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo
+
+r1 = float(input('Primeira reta: '))
+r2 = float(input('Segunda reta: '))
+r3 = float(input('terceira reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODE FORMAR trinângulo!')
+else:
+    print('Os segmentos a cima NAO PODEM FORMAR um triângulo')
