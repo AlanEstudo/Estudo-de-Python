9456736941ce433261bb61156d1afb3fe7692b15
AlanEstudo <alanjuniorestudo@gmail.com>
2021-09-14 06:55:56 -0300

    Tuplas

---
+++ aula016a.py
@@ -0,0 +1,21 @@
+# Variaveis Compostas (Tuplas)
+# Tuplas são imutaveis
+#lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+#print(sorted(lanche))
+
+#for pos, c in enumerate(lanche):
+#    print(f'Eu vou comer {c} na posição{pos}')
+
+#for c in range(0, len(lanche)):
+#    print(f'eu vou comer {lanche[c]} na posição {c}')
+
+#for c in lanche:
+#    print(f'Eu vou comer {c}')
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c.count(9))
+
+
+
