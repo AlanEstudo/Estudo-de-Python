74055c1723888d6f150650ead43b4af28493a628
AlanEstudo <alanjuniorestudo@gmail.com>
2021-09-15 06:20:13 -0300

    Números por extense v1.2

--- ex072.py
+++ ex072.py
@@ -5,10 +5,15 @@ valores_extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco',
                    'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
                    'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze',
                    'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-while True:
-    numero_usuario = int(input('Digite um número entre 0 e 20: '))
-    if 0 <= numero_usuario <= 20:
-        break
-    print('Numero incorreto! ', end='')
+continuar = ''
+while continuar != 'N':
+    while True:
+        numero_usuario = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= numero_usuario <= 20:
+            break
+        print('Numero incorreto! ', end='')
 
-print(f'Você digitou o número {valores_extenso[numero_usuario]}')
+    print(f'Você digitou o número {valores_extenso[numero_usuario]}')
+    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+
+
