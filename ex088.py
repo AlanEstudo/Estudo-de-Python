667f20c3eefdb1daf25fc2cc20a24454a00c1ba9
AlanEstudo <alanjuniorestudo@gmail.com>
2021-09-30 05:36:29 -0300

    Palpites para a Mega Sena

--- ex088.py
+++ ex088.py
@@ -1,30 +1,30 @@
-# Aprimore o desafio anterior, mostrando no final:
-# A soma de todos os valores pares digitados.
-# A soma dos valores da terceira coluna.
-# O maior valor da segunda linha.
-soma_pares = soma_coluna = maior_valor = 0
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-manipular= []
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
-print('=-' * 35)
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        print(f'[{matriz[linha][coluna]:^5}]', end='')
-        if matriz[linha][coluna] % 2 == 0:
-            soma_pares += matriz[linha][coluna]
-    print()
-print('=-' * 35)
-print(f'A soma de todos os valores pares é {soma_pares}')
-for linha in range(0, 3):
-    soma_coluna += matriz[linha][2]
-print(f'A soma dos valores da terceira coluna é {soma_coluna}')
-for coluna in range(0, 3):
-    if coluna == 0:
-        maior_valor = matriz[1][coluna]
-    elif matriz[1][coluna] > maior_valor:
-        maior_valor = matriz[1][coluna]
-print(f'O maior valor da segunda linha é {maior_valor}')
+# Faça um programa que ajuda um jogador da MEGA SENA a criar palpites.
+# O programa vai perguntar quantos jogos serão gerados
+# e vai sortear 6 números entre 1 e 60 para cada jogo,
+# cadastrando tudo em uma lista composta.
+from random import randint
+print('-' * 40)
+print('           JOGO DA MEGA SENA         ')
+print('-' * 40)
+class_jogo = []
+jogos = []
 
+tot = 1
+qnt_jogo = int(input('Quantos jogos você quer que eu sorteie? '))
+print(f'-=' * 5, f'SORTEANDO {qnt_jogo} jogos ', f'-=' * 5)
+for c in range(0, qnt_jogo):
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in class_jogo:
+            class_jogo.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    class_jogo.sort()
+    jogos.append(class_jogo[:])
+    class_jogo.clear()
+
+for indice, lista in enumerate(jogos):
+    print(f'jogo {indice + 1}: {lista}')
 
