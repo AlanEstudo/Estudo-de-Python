87a3aaac133b06aa8f00e25d7b46180fe24084f3
AlanEstudo <alanjuniorestudo@gmail.com>
2021-10-01 06:29:02 -0300

    alterado a saida da informação usando for

--- ex090.py
+++ ex090.py
@@ -6,9 +6,7 @@ dados = {}
 situacao = ''
 dados['Nome'] = str(input('Nome: '))
 dados['Media'] = float(input(f'Média da {dados["Nome"]}: '))
-print('-=' * 40)
-print(f' - Nome é igual a {dados["Nome"]}.')
-print(f' - Média é igual a {dados["Media"]}.')
+
 if dados["Media"] >= 6:
     situacao = 'Aprovado'
     dados["Situação"] = situacao
@@ -18,4 +16,8 @@ elif dados["Media"] >= 5 and dados["Media" < 6]:
 else:
     situacao = 'Reprovado'
     dados["Situação"] = situacao
-print(f' - Situação é igual a {dados["Situação"]}')
+
+print('-=' * 40)
+for k, v in dados.items():
+    print(f' - {k} é iqual a {v}')
+
